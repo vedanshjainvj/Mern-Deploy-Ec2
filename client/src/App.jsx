afee48a9d26d530233b0984b5472ae84bfcf4bc6
vedanshjainvj <vedansh.code@gmail.com>
2025-08-05 00:59:07 +0530

    Added env for frontend in github actions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-const API_BASE_URL = import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:6000/api';
 
 function App() {
   const [count, setCount] = useState(0);
